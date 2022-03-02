687acffa32eff6b82a06a4231429f75941a313d3
Carlos Mario Jaramillo <cmariojg@hotmail.com>
2022-03-02 16:23:29 -0500

    settings service multidevice

--- Services/MultiDevice.ts
+++ Services/MultiDevice.ts
@@ -7,28 +7,27 @@ import makeWASocket, {
 
 // the store maintains the data of the WA connection in memory
 // can be written out to a file & read from it
-
-const { state, saveState } = useSingleFileAuthState("./auth_info_multi.json");
-
+const clients = [];
 // start a connection
-const startSock = async () => {
+export const connectToWhatsApp = async (req: any, res: any) => {
   // fetch latest version of WA Web
   const { version, isLatest } = await fetchLatestBaileysVersion();
+
+  const { id } = req.body;
+
+  const { state, saveState } = useSingleFileAuthState(
+    `./sessions/auth_info_multi_${id}.json`
+  );
+
   console.log(`using WA v${version.join(".")}, isLatest: ${isLatest}`);
 
-  const sock = makeWASocket({
+  clients[id] = makeWASocket({
     version,
     printQRInTerminal: true,
     auth: state,
-    // implement to handle retries
-    getMessage: async (key) => {
-      return {
-        conversation: "hello",
-      };
-    },
   });
 
-  sock.ev.on("connection.update", async (update) => {
+  clients[id].ev.on("connection.update", async (update) => {
     const { connection, lastDisconnect } = update;
     if (connection === "close") {
       // reconnect if not logged out
@@ -36,24 +35,34 @@ const startSock = async () => {
         (lastDisconnect.error as Boom)?.output?.statusCode !==
         DisconnectReason.loggedOut
       ) {
-        startSock();
+        connectToWhatsApp(req, res);
       } else {
         console.log("connection closed");
       }
     }
 
     if (connection === "open") {
-      await sock.sendMessage("573135519620@s.whatsapp.net", {
-        text: "Hola!",
-      });
+      console.log("Connection Open");
     }
 
-    console.log("connection update", update);
+    // if (update?.qr) {
+    //   res.jsonp({ status: 200, qr: update.qr });
+    // }
   });
   // listen for when the auth credentials is updated
-  sock.ev.on("creds.update", saveState);
-
-  return sock;
+  clients[id].ev.on("creds.update", saveState);
 };
 
-startSock();
+export const sendMessage = async (req: any, res: any) => {
+  const { id, phone, body } = req.body;
+  // send a simple text!
+
+  const phoneDest = `${phone}@s.whatsapp.net`; // the WhatsApp ID
+  try {
+    await clients[id].sendMessage(phoneDest, { text: body });
+  } catch (err) {
+    return res.jsonp({ status: 400, error: "Error enviando el mensaje" });
+  }
+
+  return res.jsonp({ status: 200, mensaje: "Notificación enviada" });
+};
---
+++ Services/index.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const router = express.Router();
+import path from "path";
+import multer from "multer";
+const storage = multer.diskStorage({
+  destination(req: any, file: any, cb: any) {
+    cb(null, "uploads/");
+  },
+  filename(req: any, file: any, cb: any) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Appending extension
+  },
+});
+
+const upload = multer({ dest: "uploads/" });
+
+app.use(bodyParser.json());
+
+// import { conectApi, loadSession, sendFileMessage, sendMessage } from './v4'
+
+// app.post('/whatsapp/connect', conectApi)
+// app.post('/whatsapp/load_session', loadSession)
+// app.post('/whatsapp/sendmessage', sendMessage)
+// app.post('/whatsapp/sendfile', upload.single('file'), sendFileMessage)
+
+import { connectToWhatsApp, sendMessage } from "./MultiDevice";
+
+app.post("/whatsapp/connect", connectToWhatsApp);
+app.post("/whatsapp/sendmessage", sendMessage);
+// app.post("/whatsapp/load_session", loadSession);
+// app.post("/whatsapp/sendfile", upload.single("file"), sendFileMessage);
+
+app.listen(4013, () => {
+  console.log("conectado");
+});
