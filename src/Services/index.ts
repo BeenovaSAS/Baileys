19b8f606fe554157bec2096b00d8650e3c4bdccc
Carlos Mario Jaramillo <cmariojg@hotmail.com>
2024-05-07 17:14:56 -0500

    feat: new whatsapp package version

--- src/Services/index.ts
+++ src/Services/index.ts
@@ -1,40 +1,32 @@
-import bodyParser from 'body-parser'
-import * as dotenv from 'dotenv'
-import express from 'express'
-import path from 'path'
+import bodyParser from "body-parser";
+import * as dotenv from "dotenv";
+import express from "express";
+import path from "path";
+import { connectToWhatsApp, sendFileMessage, sendMessage } from "./MultiDevice";
 
+dotenv.config({ path: path.join(__dirname, "../../.env") });
+const app = express();
 
-dotenv.config({ path:path.join(__dirname, '../../.env') })
-const app = express()
-
-const router = express.Router()
-import multer from 'multer'
+const router = express.Router();
+import multer from "multer";
 const storage = multer.diskStorage({
-	destination(req: any, file: any, cb: any) {
-		cb(null, 'uploads/')
-	},
-	filename(req: any, file: any, cb: any) {
-		cb(null, Date.now() + path.extname(file.originalname)) // Appending extension
-	},
-})
+  destination(req: any, file: any, cb: any) {
+    cb(null, "uploads/");
+  },
+  filename(req: any, file: any, cb: any) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Appending extension
+  },
+});
 
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: "uploads/" });
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-
-import {
-	connectToWhatsApp,
-	sendFileMessage,
-	sendMessage,
-} from './MultiDevice'
-
-
-app.post('/whatsapp/connect', connectToWhatsApp)
-app.post('/whatsapp/load_session', connectToWhatsApp)
-app.post('/whatsapp/sendmessage', sendMessage)
-app.post('/whatsapp/sendfile', upload.single('file'), sendFileMessage)
+app.post("/whatsapp/connect", connectToWhatsApp);
+app.post("/whatsapp/load_session", connectToWhatsApp);
+app.post("/whatsapp/sendmessage", sendMessage);
+app.post("/whatsapp/sendfile", upload.single("file"), sendFileMessage);
 
 app.listen(4013, () => {
-	console.log('conectado')
-})
+  console.log("conectado");
+});
